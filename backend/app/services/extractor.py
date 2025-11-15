38ccc714d19e68773f0068b501844dfc5f9e707f
Leib <leibnroth@gmail.com>
2025-11-15 18:47:02 +0200

    standardized text for better extraction

--- backend/app/services/extractor.py
+++ backend/app/services/extractor.py
@@ -3,11 +3,13 @@ import spacy
 from nlp_data.skills import skills
 from nlp_data.job_titles import job_titles
 from nlp_data.education import get_universities
+import re
 
 class NLPExtractor():
     def __init__(self, resume):
         # Load spaCy English model
         self.resume = resume
+        self.resume_clean = re.sub(r"\s+", " ", resume.lower())
         self.nlp = spacy.load("en_core_web_sm")
         self.skills = [s.lower() for s in skills]
         self.job_titles = [j.lower() for j in job_titles]
@@ -17,7 +19,8 @@ class NLPExtractor():
         # Extracting all skills from parsed resume text
         found_skills = []
         for skill in self.skills:
-            if skill in self.resume:
+            pattern = r'\b' + re.escape(skill) + r'\b'
+            if re.search(pattern, self.resume_clean):
                 found_skills.append(skill)
         return found_skills
             
@@ -25,7 +28,8 @@ class NLPExtractor():
         # Extracting all job titles from parsed_text
         found_jobs = []
         for job_title in self.job_titles:
-            if job_title in self.resume:
+            pattern = r'\b' + re.escape(job_title) + r'\b'
+            if re.search(pattern, self.resume_clean):
                 found_jobs.append(job_title)
         return found_jobs
     
@@ -39,15 +43,18 @@ class NLPExtractor():
         # Extracting all or any universities attended4
         found_unis = []
         for uni in self.uni:
-            if uni in self.resume:
+            pattern = r'\b' + uni + r'\b'
+            if re.search(pattern, self.resume_clean):
                 found_unis.append(uni)
         return found_unis
 
 if __name__=="__main__":
     # 1. Parse PDF
-    pdf_parser = ParsePDF(r"C:/Users/leibn/Downloads/test_resume.pdf")
+    pdf_parser = ParsePDF(r"C:\Users\leibn\Downloads\Resume (1).pdf")
     parsed_text = pdf_parser.parse()
+    print(parsed_text)
     
+ 
     #2. Initialize extractor
     extractor = NLPExtractor(parsed_text)
 
@@ -59,3 +66,4 @@ if __name__=="__main__":
     print(skills_found)
     print(jobs_found)
     print(education_found)
+   
